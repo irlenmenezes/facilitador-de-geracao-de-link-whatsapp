98e3dece71ec85a239ba00fb226c199da16b68d6
Irlen Menezes <41553890+irlenmenezes@users.noreply.github.com>
2022-03-07 11:09:37 -0300

    Add files via upload

---
+++ teste-url-api-whatsapp.py
@@ -0,0 +1,7 @@
+Hotel = input('Digite o Nome do Hotel:')
+campanha_texto1 = "Olá, acabei de receber um e-mail de vocês com a promoção do Pacote de Desfile de Carnaval que inclui: Hospedagem no "
+campanha_texto2 = " com Ingressos para Desfile das Escolas de Samba e Traslado."
+texto_completo = f"{campanha_texto1}{Hotel}{campanha_texto2}"
+link_whatsapp ="https://api.whatsapp.com/send/?phone=5521991192267&text=" 
+url_completa_whatsapp = f"{link_whatsapp}{texto_completo}"
+print(url_completa_whatsapp)
